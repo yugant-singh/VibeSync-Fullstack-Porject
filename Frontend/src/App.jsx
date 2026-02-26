fe30cacf8c0f34f97a6fb03ad77e3b7008e190c4
Yugant Singh <yugantsingh9651@gmail.com>
2026-02-26 12:37:59 +0530

    Step 1 Complete Face Expression detection using Google Media pipe

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import FaceExpression from "./features/expression/pages/FaceExpression";
+
+
+
+function App() {
+  return (
+    <>
+   <FaceExpression/>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/features/expression/pages/FaceExpression.jsx
@@ -0,0 +1,110 @@
+import { useEffect, useRef, useState } from "react";
+import {
+    FaceLandmarker,
+    FilesetResolver
+} from "@mediapipe/tasks-vision";
+
+export default function FaceExpression() {
+    const videoRef = useRef(null);
+    const landmarkerRef = useRef(null);
+    const animationRef = useRef(null);
+    let stream;
+
+    const [ expression, setExpression ] = useState("Detecting...");
+
+    const init = async () => {
+        const vision = await FilesetResolver.forVisionTasks(
+            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+        );
+
+        landmarkerRef.current = await FaceLandmarker.createFromOptions(
+            vision,
+            {
+                baseOptions: {
+                    modelAssetPath:
+                        "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/latest/face_landmarker.task"
+                },
+                outputFaceBlendshapes: true,
+                runningMode: "VIDEO",
+                numFaces: 1
+            }
+        );
+
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        videoRef.current.srcObject = stream;
+        await videoRef.current.play();
+
+        detect();
+    };
+
+    const detect = () => {
+        if (!landmarkerRef.current || !videoRef.current) return;
+
+        const results = landmarkerRef.current.detectForVideo(
+            videoRef.current,
+            performance.now()
+        );
+
+        if (results.faceBlendshapes?.length > 0) {
+            const blendshapes = results.faceBlendshapes[ 0 ].categories;
+
+            const getScore = (name) =>
+                blendshapes.find((b) => b.categoryName === name)?.score || 0;
+
+            const smileLeft = getScore("mouthSmileLeft");
+            const smileRight = getScore("mouthSmileRight");
+            const jawOpen = getScore("jawOpen");
+            const browUp = getScore("browInnerUp");
+            const frownLeft = getScore("mouthFrownLeft");
+            const frownRight = getScore("mouthFrownRight");
+
+            console.log(getScore("jawOpen"))
+
+            let currentExpression = "Neutral";
+
+            if (smileLeft > 0.5 && smileRight > 0.5) {
+                currentExpression = "Happy 😄";
+            } else if (jawOpen > 0.2 && browUp > 0.2) {
+                currentExpression = "Surprised 😲";
+            } else if (frownLeft > 0.0001 && frownRight > 0.0001) {
+                currentExpression = "Sad 😢";
+            }
+
+            setExpression(currentExpression);
+        }
+    };
+
+    useEffect(() => {
+        
+
+        init();
+
+        return () => {
+            if (animationRef.current) {
+                cancelAnimationFrame(animationRef.current);
+            }
+
+            if (landmarkerRef.current) {
+                landmarkerRef.current.close();
+            }
+
+            if (videoRef.current?.srcObject) {
+                videoRef.current.srcObject
+                    .getTracks()
+                    .forEach((track) => track.stop());
+            }
+        };
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <video
+                ref={videoRef}
+                style={{ width: "400px", borderRadius: "12px" }}
+                playsInline
+            />
+            <h2>{expression}</h2>
+            <button onClick={detect} >Detect expression</button>
+        </div>
+    );
+}
