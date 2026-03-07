1b1e2a03a889185f908ffe277bd7e4d195552e87
yugant-singh <yugantsingh9651@gmail.com>
2026-03-07 11:03:22 +0530

    make logout and profile page

--- Frontend/src/features/expression/utils/utils.js
+++ Frontend/src/features/expression/utils/utils.js
@@ -53,11 +53,11 @@ export const detect = ({ landmarkerRef, videoRef, setExpression }) => {
         let currentExpression = "Neutral";
 
         if (smileLeft > 0.5 && smileRight > 0.5) {
-            currentExpression = "Happy 😄";
+            currentExpression = "happy";
         } else if (jawOpen > 0.2 && browUp > 0.2) {
-            currentExpression = "Surprised 😲";
+            currentExpression = "surprised";
         } else if (frownLeft > 0.0001 && frownRight > 0.0001) {
-            currentExpression = "Sad 😢";
+            currentExpression = "sad";
         }
 
         setExpression(currentExpression);
--- Frontend/src/shared/components/Navbar.jsx
+++ Frontend/src/shared/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import { useAuth } from '../../features/auth/hooks/useAuth'
 import "../components/navbar.scss"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { user, handleLogout } = useAuth()
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const navigate = useNavigate()
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setDropdownOpen(false)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [])
+
 
   return (
     <nav className="navbar">
@@ -40,6 +56,31 @@ const Navbar = () => {
           </li>
         </ul>
 
+        {/* Avatar + Dropdown */}
+        <div ref={dropdownRef} className="navbar__avatar"
+          onClick={() => setDropdownOpen(!dropdownOpen)}>
+
+          {/* User ka pehla letter */}
+          <span className="navbar__avatar-letter">
+            {user?.name?.charAt(0).toUpperCase()}
+          </span>
+
+          {/* Dropdown */}
+          {dropdownOpen && (
+            <div className="navbar__dropdown">
+              <p className="navbar__dropdown-name">{user?.name}</p>
+              <hr />
+              <button onClick={() => navigate('/profile')}>
+                👤 Profile
+              </button>
+              <button onClick={handleLogout}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 22C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V6H18V4H6V20H18V18H20V21C20 21.5523 19.5523 22 19 22H5ZM18 16V13H11V11H18V8L23 12L18 16Z"></path></svg> Logout
+              </button>
+            </div>
+          )}
+
+        </div>
+
         {/* Hamburger (Mobile) */}
         <button
           className={`navbar__hamburger ${menuOpen ? "open" : ""}`}
@@ -60,6 +101,15 @@ const Navbar = () => {
           <li><NavLink to="/history" onClick={() => setMenuOpen(false)}>History</NavLink></li>
           <li><NavLink to="/profile" onClick={() => setMenuOpen(false)}>Profile</NavLink></li>
         </ul>
+        <div className="navbar__mobile-user">
+          <span className="navbar__mobile-avatar">
+            {user?.name?.charAt(0).toUpperCase()}
+          </span>
+          <p>{user?.name}</p>
+      <button onClick={handleLogout}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 22C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V6H18V4H6V20H18V18H20V21C20 21.5523 19.5523 22 19 22H5ZM18 16V13H11V11H18V8L23 12L18 16Z"></path></svg> Logout
+              </button>
+        </div>
       </div>
     </nav>
   );
